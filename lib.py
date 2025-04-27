66e4f38ae7b95b089baf34f54eb1287987ced432
Sean Liu <zxliu2@illinois.edu>
2025-04-26 22:49:53 -0500

    move functions out of class

--- lib.py
+++ lib.py
@@ -4,7 +4,15 @@ from typing import List
 import os
 from collections import Counter
 from pathlib import Path
+from copy import deepcopy
 
+
+def get_parent(tree, node):
+    path = [tree.root] + tree.get_path(node)
+    if len(path) <= 1:
+        raise ValueError(f"Could not find parent of {node=}, are you sure {node=} is a valid non-root node?")
+    parent = path[-2]
+    return parent
 
 class QuartetPolytomy: 
     def get_new_name(self):
@@ -52,19 +60,14 @@ class QuartetPolytomy:
         # stitch things together
 
 
-    def get_parent(self, node):
-        path = [self.tree.root] + self.tree.get_path(node)
-        if len(path) <= 1:
-            raise ValueError(f"Could not find parent of {node=}, are you sure {node=} is a valid non-root node?")
-        parent = path[-2]
-        return parent
+
 
     def precompute_label_map(self):
         poly_label_map = { 
             polytomy.name: {} for polytomy in self.polytomies 
         }
         for poly in self.polytomies:
-            parent = self.get_parent(poly)
+            parent = get_parent(self.tree, poly)
 
             for child in poly.clades: 
                 leaves = list(child.find_elements(terminal=True))
@@ -115,8 +118,11 @@ class QuartetPolytomy:
             # what happens if the leafset is not fully determined? for neighbours a,b,c,d add 
             # ab|cd, ac|bd, and ad|bc so that no bias
             # and make sure each leaf is in at least one quartet.
-            with open(polytomy_folder / 'quartets.nwk', 'w') as qf:
-                poly_neighbours = list(set(self.poly_label_map[polytomy.name].values())) # 
+            with open(polytomy_folder / 'quartets.nwk', 'w') as qf, open(polytomy_folder / 'metadata.csv', 'w') as mdf:
+                poly_neighbours = list(set(self.poly_label_map[polytomy.name].values()))
+
+                mdf.write(f"POLYTOMY SIZE {len(poly_neighbours)}\n")
+
                 for i in range(len(poly_neighbours) - 3):
                     a, b, c, d = poly_neighbours[i:i+4]
                     qf.write(f'(({a},{b}),({c},{d}));\n')
@@ -125,6 +131,55 @@ class QuartetPolytomy:
 
                 for (a, b, c, d), w in self.polytomy_quartets[polytomy.name].items():
                     qf.write(f'(({a},{b}),({c},{d}));\n' * w)
+        
+    
+def resolve_one_polytomy(
+    tree_pt, # tree with polytomy
+    pt_node_name, # node name of the polytomy to resolve
+    resolution_tree, # the resolution tree such that the leafset of this tree is exactly the neighbours of pt_node_name in tree_pt
+): 
+    """ArithmeticError
+    Input:
+    tree_pt, tree with polytomy
+    pt_node_name, node name of the polytomy to resolve
+    resolution_tree, the resolution tree such that the leafset of this tree is exactly the neighbours of pt_node_name in tree_pt
+    Output: 
+    tree_pt with the resolution tree in place of the original polytomy at pt_node_name. 
+    Tree WILL have unifurcations, but they are intended and should be fixed AFTER all polytomies are fixed, as they help to represent nodes that are may no longer present.
+    Will not chagne resolution_tree, but CHANGES tree_pt IN PLACE
+    """
+    # assumptions: tree_pt
+    # 
+    old_resolution_tree = deepcopy(resolution_tree)
+    assert len(list(tree_pt.find_clades(target=pt_node_name))) == 1, f"found multiple nodes with name {pt_node_name}"
+    pt_node = next(iter(tree_pt.find_clades(target=pt_node_name)))
+    parent = get_parent(tree_pt, pt_node)
+    assert parent is not None, "parent of polytomy should not be none."
+    assert set([parent] + [ clade.name for clade in pt_node.clades ]) ==\
+        set([ leaf.name for leaf in resolution_tree.find_elements(terminal=True) ] ),\
+        "Resolution leaf set is not the same as the neighbours of the polytomy"
+    # stitch the children the original polytomy on the resolution
+    for child in pt_node.clades:
+        name = child.name 
+        assert len(list(resolution_tree.find_clades(target=name, terminal=True))) == 1, f"Found multiple or no nodes in the resolution with name {name}"
+        resolution_child = next(iter(resolution_tree.find_clades(target=name, terminal=True)))
+        resolution_child.clades = [child]
+        resolution_child.name = pt_node.name
+        print(resolution_child, resolution_child.clades)
+
+    # replace the parent -> polytomy node with the tree in tree_pt
+    resolution_tree.root_with_outgroup([parent.name])
+    resolution_tree.prune(parent.name)
+    for i, c in enumerate(parent.clades):
+        if c.name == pt_node.name:
+            parent.clades[i] = resolution_tree.root
+            break
+
+    resolution_tree = old_resolution_tree
+    return tree_pt
+
+
+    
             
 
 
